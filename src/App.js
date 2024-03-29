aca7d6f8ea2edcd89f637d503ea6e9f6b4e8bf39
Hamzah Fayad <hamzahfayad99@gmail.com>
2024-03-29 13:49:56 +0100

    #

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
 import "./App.css";
 import Nav from "./Nav";
 
+let features = [
+  {
+    id: 0,
+    img: "./images/logos/vloov.png",
+  },
+  {
+    id: 1,
+    img: "./images/logos/vloov.png",
+  },
+  {
+    id: 2,
+    img: "./images/logos/vloov.png",
+  },
+];
+
 function App() {
   return (
     <div className="App">
@@ -12,8 +27,24 @@ function App() {
             THE WAY TO FLOW .
           </h1>
         </div>
-        <p>Lorem Ipsum</p>
-        <img src="./images/logos/vloov.png" />
+        <div className="infos text-wrap gap">
+          <p>
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit.
+          </p>
+        </div>
+        <div className="features-img gap">
+          {features.map((f) => {
+            return <img src={f.img} alt={"img-" + f.id} key={"img-" + f.id} />;
+          })}
+        </div>
       </div>
     </div>
   );
